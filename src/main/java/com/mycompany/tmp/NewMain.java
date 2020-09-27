628749aa0e845224cd4bba8a3f4974be3f8bdb89
mchinchole <michael.chinchole@safety-line.fr>
2020-09-27 18:02:55 +0200

    presque fini

--- src/main/java/com/mycompany/tmp/NewMain.java
+++ src/main/java/com/mycompany/tmp/NewMain.java
@@ -14,33 +14,45 @@ public class NewMain {
             System.out.println("nombe de frames = " + nsc2);
             System.out.println("temps en seconde = " + nsc2 / 44100);
 
-            double[] buffer = new double[nsc2 * 2 > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) nsc2 * 2]; //x2 pour stocker la valeure gauche et droite décodé
+            double[] buffer = new double[nsc2 * wavFile.getNumChannels() > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) nsc2 * wavFile.getNumChannels()]; //*wavFile.getNumChannels() pour stocker la valeure gauche et droite décodé
             final int SAMPLE = buffer.length / 2;
 
             int sampleCount = 1;
 
-            double gauche;
-            double droite;
-            int nbSong = 1;
+            double gauche = 0d;
+            double droite = 0d;
+            int nbSong = 0;
+
             while (sampleCount > 0) {
                 sampleCount = wavFile.readFrames(buffer, SAMPLE);
                 System.out.println("sampleCount lu=" + sampleCount);
+                int previous = 0;
                 for (int i = 0; i < sampleCount; i++) {
                     gauche = buffer[i * 2];
-                    droite = buffer[i * 2 + 1];
-                    if (gauche == 0f && droite == 0f) {
+                    if (wavFile.getNumChannels() == 2) {
+                        droite = buffer[i * 2 + 1];
+                    }
+                    if ((gauche == 0d && droite == 0d) || (i + 1 >= sampleCount)) { // (i + 1 == sampleCount) => fin de chanson
                         int nbBlank = 0;
-                        for (int j = i; j < sampleCount; j++, i++) {
-                            gauche = buffer[j * 2];
-                            droite = buffer[j * 2 + 1];
-                            if (gauche == 0f && droite == 0f) {
+                        for (int j = i; j < sampleCount; j++, i++) { //detecter le nombre de silance
+                            gauche = buffer[j * wavFile.getNumChannels()];
+                            if (wavFile.getNumChannels() == 2) {
+                                droite = buffer[j * wavFile.getNumChannels() + 1];
+                            }
+                            if (gauche == 0d && droite == 0d) {
                                 nbBlank++;
                             } else {
                                 break;
                             }
                         }
-                        if (nbBlank > 21000) { //21000 < 500ms                       
+                        if (nbBlank > 21000 || (i + 1 >= sampleCount)) { //21000 < 500ms  || fin de chanson
                             nbSong++;
+                            File file = new File("C:\\ffmpeg\\test\\carpenter-brut-trilogy-" + nbSong + ".wav");
+                            file.createNewFile();
+                            WavFile wf = WavFile.newWavFile(file, wavFile.getNumChannels(), i - previous, wavFile.getValidBits(), wavFile.getSampleRate());
+                            wf.writeFrames(buffer, previous, i - previous);
+                            wf.close();
+                            previous = i;
                         }
                     }
                 }
@@ -51,16 +63,5 @@ public class NewMain {
         } catch (Exception e) {
             System.err.println(e);
         }
-    }
-
-    private static boolean have0after(int i, double buffer[], int limit) {
-        if (i >= limit) {
-            return false;
-        }
-        int j = i + 1;
-        double gauche = buffer[j * 2];
-        double droite = buffer[j * 2 + 1];
-        return gauche == 0f && droite == 0f;
-
     }
 }
